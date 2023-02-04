134b9fb34df18359e0285de38a76c156c2d8bb97
TanTeckLing <2102072@sit.singaporetech.edu.sg>
2023-02-04 23:27:22 +0800

    Added location (incomplete) permission requesting using google location api

--- app/src/main/java/com/example/project/tecklingActivity.java
+++ app/src/main/java/com/example/project/tecklingActivity.java
@@ -2,13 +2,26 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class tecklingActivity extends AppCompatActivity {
 
+    Button locationButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teckling);
+
+        locationButton = (Button) findViewById(R.id.LOCATION);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LocationService.class);
+                startActivity(intent);
+            }
+        });
     }
 }
