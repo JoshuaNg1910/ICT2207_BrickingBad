a21b658074439b333f9bc0edb513dbaa531c25dc
JoshuaNg1910 <2100929@sit.singaporetech.edu.sg>
2023-02-09 00:33:42 +0800

    Revert "BLESS THE BOIS"
    This reverts commit dc62ed6958b91bff8d3ec113bc4b34f71204a720.

--- app/src/main/java/com/example/project/DBHelper.java
+++ app/src/main/java/com/example/project/DBHelper.java
@@ -1,4 +1,3 @@
-
 package com.example.project;
 import android.content.ContentValues;
 import android.content.Context;
@@ -6,48 +5,22 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import java.util.ArrayList;
-
 public class DBHelper extends SQLiteOpenHelper {
     public static final String DBNAME = "Login.db";
     public DBHelper(Context context) {
         super(context, "Login.db", null, 1);
     }
-    private ArrayList<String> messages = new ArrayList<>();
-
 
     @Override
     public void onCreate(SQLiteDatabase MyDB) {
         MyDB.execSQL("CREATE TABLE users(username TEXT PRIMARY KEY, password TEXT, profilepic BLOB)");
-        MyDB.execSQL("CREATE TABLE messages(message TEXT PRIMARY KEY)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase MyDB, int i, int i1) {
         MyDB.execSQL("DROP TABLE IF EXISTS users");
-        MyDB.execSQL("DROP TABLE IF EXISTS messages");
-    }
-    public boolean insertMessage(String message){
-        SQLiteDatabase MyDB = this.getWritableDatabase();
-        ContentValues contentValues= new ContentValues();
-        contentValues.put("message", message);
-        long results = MyDB.insert("messages", null, contentValues);
-        if(results == -1)
-            return false;
-        else
-            return true;
     }
 
-    public ArrayList<String> loadMessages() {
-        SQLiteDatabase MyDB = this.getReadableDatabase();
-        Cursor cursor = MyDB.rawQuery("SELECT * FROM messages", null);
-        if (cursor!=null && cursor.moveToFirst()) {
-            do {
-                messages.add(cursor.getString(cursor.getColumnIndexOrThrow("message")));
-            } while (cursor.moveToNext());
-        }
-        return messages;
-    }
     public Boolean insertData(String username, String password, byte[] dp){
         SQLiteDatabase MyDB = this.getWritableDatabase();
         ContentValues contentValues= new ContentValues();
@@ -89,4 +62,3 @@ public class DBHelper extends SQLiteOpenHelper {
         return dp;
     }
 }
-
