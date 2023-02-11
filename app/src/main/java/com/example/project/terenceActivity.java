2c9c4ad2f74e158c3c56ec0b28daf5053f12b019
Terence2389 <94297073+Terence2389@users.noreply.github.com>
2023-02-11 16:21:00 +0800

    WHY

--- app/src/main/java/com/example/project/terenceActivity.java
+++ app/src/main/java/com/example/project/terenceActivity.java
@@ -1,20 +1,21 @@
 package com.example.project;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Toast;
 import android.widget.Button;
 
 import java.io.File;
@@ -23,7 +24,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class terenceActivity extends AppCompatActivity {
+public class terenceActivity extends AppCompatActivity{
 
     static final int REQUEST_IMAGE_CAPTURE = 100;
     String mCurrentPhotoPath;
@@ -37,16 +38,39 @@ public class terenceActivity extends AppCompatActivity {
         Take.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(terenceActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(terenceActivity.this, new String[]{Manifest.permission.CAMERA}, 0);
-                } /*else if (ContextCompat.checkSelfPermission(terenceActivity.this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(terenceActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
-                }*/  else if (ContextCompat.checkSelfPermission(terenceActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(terenceActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
-                }else {
-                    StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-                    StrictMode.setVmPolicy(builder.build());
-                    dispatchTakePictureIntent();
+
+                if(ContextCompat.checkSelfPermission(terenceActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                        || ContextCompat.checkSelfPermission(terenceActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(terenceActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(terenceActivity.this, Manifest.permission.CAMERA)
+                            || ActivityCompat.shouldShowRequestPermissionRationale(terenceActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    || ActivityCompat.shouldShowRequestPermissionRationale(terenceActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE))
+                    {
+                        ActivityCompat.requestPermissions(terenceActivity.this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+                    }
+                } else {
+                    final CharSequence[] options = {"Take Photo from Camera", "Choose from Gallery", "Cancel"};
+                    AlertDialog.Builder builder = new AlertDialog.Builder(terenceActivity.this);
+                    builder.setTitle("Choose your profile picture");
+                    builder.setItems(options, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int item) {
+                            if (options[item].equals("Take Photo from Camera")) {
+                                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                                StrictMode.setVmPolicy(builder.build());
+                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(intent, 100);
+                                //dispatchTakePictureIntent();
+                            } else if (options[item].equals("Choose from Gallery")) {
+                                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                //intent.setType("image/*");
+                                startActivityForResult(intent, 200);
+                            } else if (options[item].equals("Cancel")) {
+                                dialog.dismiss();
+                            }
+                        }
+                    });
                 }
             }
         });
@@ -91,8 +115,19 @@ public class terenceActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
             galleryAddPic();
+        }
+        else if (requestCode == 200){
+            if(data != null && data.getData() != null){
+                try{
+                    Uri image = data.getData();
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), image);
+                    //circleView.setImageBitmap(bitmap);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
