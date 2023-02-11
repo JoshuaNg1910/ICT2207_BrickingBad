ad8590f97f49c8fa4fbaa9f09e197cc6aa6b44bb
BransonTian <2101848@sit.singaporetech.edu.sg>
2023-02-11 14:44:23 +0800

    static toolbar with back button
    Back button image, frame created for dp and name of user

--- app/src/main/java/com/example/project/ChatTest2Activity.java
+++ app/src/main/java/com/example/project/ChatTest2Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 
 public class ChatTest2Activity extends AppCompatActivity {
     DBHelper DB;
-    Button back;
+    ImageButton back;
     private ArrayList<String> chats = new ArrayList<>();
     private ArrayAdapter<String> adapter;
     private ListView chatList;
